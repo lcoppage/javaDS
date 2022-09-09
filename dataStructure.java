d06ca4248c2db755b5fe879256504f978d8daaae
Engineering <Engineering@308-12.cls-edu.charlottelatin.org>
2022-09-09 13:31:25 -0400

    linked list

--- dataStructure.java
+++ dataStructure.java
@@ -2,7 +2,10 @@ public class dataStructure {
     public static void main(String[] args){
     linkedList llist = new linkedList();
     llist.add(5);
-    System.out.println(llist.head.data);
-    llist.printItems();    
+    llist.add(6);
+    llist.add(7);
+    System.out.println(llist.getAtIndex(2).data);
+    System.out.println(llist.getAtIndex(5));
+    System.out.println(llist.getAtIndex(0).data);
     }
 }
